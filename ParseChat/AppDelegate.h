0bab3dd8e794fb25e718e57149103f7e4864acd2
Liz Chaddock <lizchaddock@bloodjourney-lm.corp.yahoo.com>
2015-09-16 13:42:38 -0700

    Initial Commit

---
+++ ParseChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParseChat
+//
+//  Created by Liz Chaddock on 9/16/15.
+//  Copyright (c) 2015 Liz Chaddock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
